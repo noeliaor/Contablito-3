e1a80382efa3200b07432df553c6c0ddf7c1558f
Noelia Olivera Rodríguez <noeliaoliverarodriguez@gmail.com>
2021-09-30 10:05:08 -0300

    Pantalla de ingreso y visualización con estilos.
    Problema: almacenamiento de datos

--- Contablito/script.js
+++ Contablito/script.js
@@ -1,47 +1,50 @@
 const buttonIngresar = () => {
     let tbody = document.getElementById("toinformation");
     let idproduct = document.getElementById("ProductDescription").value; //Id del producto
-    document.getElementById("ProductDescription").value="";
+    document.getElementById("ProductDescription").value = "";
     let subtotal = document.getElementById("subtotal").value;
-    document.getElementById("subtotal").value="";
+    document.getElementById("subtotal").value = "";
     let type;
     let IVA;
     let total;
-
-    //Verifico si es compra o venta
-    if (document.getElementById('venta').checked) {
-        type = document.getElementById('venta').value;
-    } else {
-        type = document.getElementById('compra').value;
-    }
-
     //Busco nombre del producto
-    let productsids=[394,395,396,397];
+    let productsids = [394, 395, 396, 397,400,401,402,403];
+    let productsnames = ["Tornillo 2cm", "Tornillo 3cm", "Tornillo 4cm", "Tornillo 5cm", "Ladrillo común","Ladrillo hueco","Ladrillo refractario","Ladrillo de vidrio"];
+    let precios = [20, 30, 40, 50,200,390,418,540];
+    let stocks = [400, 560, 280, 390,500,300,400,1000];
 
-let productsnames=["Tornillo 2cm","Tornillo 3cm","Tornillo 4cm","Tornillo 5cm"];    
-    for (var i = 0; i < productsids.length; i++){ 
-        
-    if (parseInt(idproduct)==productsids[i]){
-alert("Encontrado");
-        nameproduct==productsnames[i];
+    for (var i = 0; i < productsids.length; i++) {
+        tbody.innerHTML += `<tr><td>${productsnames[i]}</td><td>${productsids[i]}</td><td>${JSON.stringify(productsids[i]).substring(2, 7)}</td><td>${precios[i]}</td><td>${stocks[i]}</td></tr>`;
     }
-    }
-
-//Cálculo del total según IVA
-    if (document.getElementById('basico').checked) {
+    //Cálculo del total según IVA
+  /*  if (document.getElementById('basico').checked) {
         IVA = 22 / 100 * subtotal;
         total = parseInt(subtotal) + IVA;
     } else if (document.getElementById('minimo').checked) {
         IVA = 10 / 100 * subtotal;
-        total = parseInt(subtotal) + IVA ;
+        total = parseInt(subtotal) + IVA;
     } else {
-        IVA=0;
+        IVA = 0;
         total = parseInt(subtotal);
-    }
-  // Ingresar un producto  
+    }*/
+    // Ingresar un producto  
     if (idproduct && subtotal) {
-        tbody.innerHTML += `<tr><td>${nameproduct}</td><td>${idproduct}</td><td>${type}</td><td>${subtotal}</td><td>${IVA}</td></tr>`;
-    } else {     
+        for (var i = 0; i < productsids.length; i++) {
+            if (parseInt(idproduct) == productsids[i]) {
+                if (document.getElementById('venta').checked) {
+                    type = document.getElementById('venta').value;
+                    stocks[i] -= parseInt(subtotal);
+                } else {
+                    type = document.getElementById('compra').value;
+                    stocks[i] += parseInt(subtotal);
+                }
+                tbody.innerHTML = "";
+                for (var i = 0; i < productsids.length; i++) {
+                    tbody.innerHTML += `<tr><td>${productsnames[i]}</td><td>${productsids[i]}</td><td>${JSON.stringify(productsids[i]).substring(2, 7)}</td><td>${precios[i]}</td><td>${stocks[i]}</td></tr>`;
+                }
+            }
+        }
+    } else {
         alert("No deben quedar campos vacíos");
     }
 };
