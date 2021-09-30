19e5bcdda88c9a9f09db57fdf0b1487a6b777995
Noelia Olivera Rodríguez <noeliaoliverarodriguez@gmail.com>
2021-09-30 10:52:01 -0300

    Update script.js

--- Contablito/script.js
+++ Contablito/script.js
@@ -1,35 +1,40 @@
-const buttonIngresar = () => {
+document.addEventListener("DOMContentLoaded", () => {
     let tbody = document.getElementById("toinformation");
-    let idproduct = document.getElementById("ProductDescription").value; //Id del producto
-    document.getElementById("ProductDescription").value = "";
-    let subtotal = document.getElementById("subtotal").value;
-    document.getElementById("subtotal").value = "";
-    let type;
-    let IVA;
-    let total;
-    //Busco nombre del producto
-    let productsids = [394, 395, 396, 397,400,401,402,403];
-    let productsnames = ["Tornillo 2cm", "Tornillo 3cm", "Tornillo 4cm", "Tornillo 5cm", "Ladrillo común","Ladrillo hueco","Ladrillo refractario","Ladrillo de vidrio"];
-    let precios = [20, 30, 40, 50,200,390,418,540];
-    let stocks = [400, 560, 280, 390,500,300,400,1000];
+      document.getElementById("ProductDescription").value = "";
 
-    for (var i = 0; i < productsids.length; i++) {
-        tbody.innerHTML += `<tr><td>${productsnames[i]}</td><td>${productsids[i]}</td><td>${JSON.stringify(productsids[i]).substring(2, 7)}</td><td>${precios[i]}</td><td>${stocks[i]}</td></tr>`;
+    let productslist= [
+        { id: 394, name: "Tornillo 2cm", price:20, stock:400},
+        { id: 395, name: "Tornillo 3cm", price:30, stock:560},
+        { id: 396, name: "Tornillo 4cm", price:40, stock:280},
+        { id: 397, name: "Tornillo 5cm", price:50, stock:390},
+        { id: 400, name: "Ladrillo común", price:200, stock:500},
+        { id: 401, name: "Ladrillo hueco", price:390, stock:300},
+        { id: 402, name: "Ladrillo refractario", price:418, stock:400},
+        { id: 403, name: "Ladrillo de vidrio", price:540, stock:1000},
+       
+    ];
+    document.getElementById("subtotal").value = "";
+  
+    localStorage.setItem("productslistSave", JSON.stringify(productslist));
+
+    for (var i = 0; i < productslist.length; i++) {
+        tbody.innerHTML += `<tr><td>${productslist[i].name}</td><td>${productslist[i].id}</td><td>${JSON.stringify((productslist[i].id)).substring(2, 0)}</td><td>${productslist[i].price}</td><td>${productslist[i].stock}</td></tr>`;
     }
-    //Cálculo del total según IVA
-  /*  if (document.getElementById('basico').checked) {
-        IVA = 22 / 100 * subtotal;
-        total = parseInt(subtotal) + IVA;
-    } else if (document.getElementById('minimo').checked) {
-        IVA = 10 / 100 * subtotal;
-        total = parseInt(subtotal) + IVA;
-    } else {
-        IVA = 0;
-        total = parseInt(subtotal);
-    }*/
-    // Ingresar un producto  
-    if (idproduct && subtotal) {
-        for (var i = 0; i < productsids.length; i++) {
+    localStorage.setItem("totalcompras", JSON.stringify(0));
+    localStorage.setItem("totalventas", JSON.stringify(0));
+   document.getElementById("btnIngresar").addEventListener("click", buttonIngresar);
+});
+
+const buttonIngresar = () => {
+
+    let type;
+    let idproduct = document.getElementById("ProductDescription").value; //Id del producto
+    let subtotal = document.getElementById("subtotal").value;
+
+    if (idproduct && subtotal) {  
+        
+        for (var i = 0; i < productsids.length; i++) {  
+              
             if (parseInt(idproduct) == productsids[i]) {
                 if (document.getElementById('venta').checked) {
                     type = document.getElementById('venta').value;
@@ -45,12 +50,24 @@ const buttonIngresar = () => {
             }
         }
     } else {
-        alert("No deben quedar campos vacíos");
+     //   alert("No deben quedar campos vacíos");
     }
+
+    //    let IVA;
+  //  let total;
+    //Busco nombre del producto
+   
+    //Cálculo del total según IVA
+  /*  if (document.getElementById('basico').checked) {
+        IVA = 22 / 100 * subtotal;
+        total = parseInt(subtotal) + IVA;
+    } else if (document.getElementById('minimo').checked) {
+        IVA = 10 / 100 * subtotal;
+        total = parseInt(subtotal) + IVA;
+    } else {
+        IVA = 0;
+        total = parseInt(subtotal);
+    }*/
+    // Ingresar un producto  
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-    localStorage.setItem("totalcompras", JSON.stringify(0));
-    localStorage.setItem("totalventas", JSON.stringify(0));
-    document.getElementById("btnIngresar").addEventListener("click", buttonIngresar);
-});
