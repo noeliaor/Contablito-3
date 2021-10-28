ea87c6e731480d7c476e959b7f8d63782c962968
Noelia Olivera Rodríguez <noeliaoliverarodriguez@gmail.com>
2021-10-27 22:31:01 -0300

    27/10 Actualización
    Posibilidad de eliminar productos. Resta arreglar ingreso de nuevos productos

--- js/formscript.js
+++ js/formscript.js
@@ -1,70 +1,68 @@
 document.addEventListener("DOMContentLoaded", () => {
     let content = `<option selected value="0"> Elige una opción </option>`
     let index = 0;
-    let categoriesids=[];
+    let categoriesids = [];
     let categorieslist = JSON.parse(localStorage.getItem("categorieslistSave"));
-    for (let item of categorieslist) {
-        categoriesids[index]=item.idpadre;
-        content += `<option value="${item.idpadre}">${item.name}</option>`
+    for (let item of categorieslist) { //Recorro items en la lista de categorías
+        categoriesids[index] = item.idpadre; //Almaceno en lista los id's padres existentes 
+        content += `<option value="${item.idpadre}">${item.name}</option>` //Cada categoría se guarda como opción
         index += 1;
-        alert(index)
     }
-    content += `<option value="new">Nueva categoría</option>`
+    content += `<option value="new">Nueva categoría</option>` //Opción de nueva categoría
     content += `</optgroup>`;
     document.getElementById("tocategories").innerHTML = content;
-    
-    document.getElementById("tocategories").addEventListener("change", function () {
+
+
+
+    document.getElementById("btnGuardar").addEventListener("click", function () {
+ 
         let categorytoproduct = document.getElementById("tocategories").value;
-        if(categorytoproduct=="new"){
-            document.getElementById("newcategory").innerHTML=`<div class="col-auto">
+    
+         if (categorytoproduct == "new") { //Si nueva categoría entonces espaco para nombre
+            document.getElementById("newcategory").innerHTML = `<div class="col-auto">
             <p class="font-weight-bold my-2">Ingrese el nombre de la nueva categoría:</p>
         </div>
         <div class="col-auto">
             <input class="form-control" type="search" id="newCategoryName" style="width:100px">
         </div>`
-         categorytoproduct.value=Math.max(categoriesids);
-         alert(categoriesids)
-         categorieslist.push({ id: categorytoproduct.value, name:document.getElementById("newCategoryName").value});
-         
-         localStorage.setItem("categorieslistSave", JSON.stringify(categorieslist)); //Guardo la nueva vista
+        categorytoproduct.value = Math.max.apply(0,categoriesids)+1;//Asigno como id de categoría al máximo ID más uno
+        categorieslist.push({idpadre=categorytoproduct.value ,name:document.getElementById("newCategoryName").value});
+              localStorage.setItem("categorieslistSave", JSON.stringify(categorieslist)); //Guardo la nueva lista de categorías
 
+       //     let categorytoproduct = document.getElementById("tocategories").value;
+         //   let listofproducts = JSON.parse(localStorage.getItem("productslistSave"));
+           // let newname = document.getElementById("newProductName").value;
+            //let newID = parseInt(JSON.stringify(categorytoproduct) + JSON.stringify(document.getElementById("IDnewProduct").value));
+            //let newminstock = parseInt(document.getElementById("NewminStock").value);
+            //let newstock = parseInt(document.getElementById("initialCount").value);
+            //let newcost = parseInt(document.getElementById("newCost").value);
+
+
+          //  if (listofproducts && newname && categorytoproduct && newID && newminstock && newstock && newcost) //Si todos los espacios completos
+            //{
+              //  if (findId(newID, listofproducts)) { //Si ID no se repite
+                //    listofproducts.push({ id: newID, name: newname, price: newcost, stock: newstock, minstock: newminstock });
+                  //  localStorage.setItem("productslistSave", JSON.stringify(listofproducts)); //Guardo la nueva vista
+
+               // } else {
+                 //   alert("Ya existe un producto con este ID, ingrese otro.")
+             //   }
+          //  } else {
+          //      console.log(categorytoproduct, listofproducts, newname, newID, newminstock, newstock, newcost);
+            //    alert("Complete todos los campos.");
+            //}
         }
-    });
-    document.getElementById("btnGuardar").addEventListener("click", function () {
-        let listofproducts = JSON.parse(localStorage.getItem("productslistSave"));
-        let newname = document.getElementById("newProductName").value;
-        let newID = parseInt(categorytoproduct + document.getElementById("IDnewProduct").value);
-        let newminstock = parseInt(document.getElementById("NewminStock").value);
-        let newstock = parseInt(document.getElementById("initialCount").value);
-        let newcost = parseInt(document.getElementById("newCost").value);
-        
+     });
 
-        if (listofproducts && newname && categorytoproduct && newID && newminstock && newstock && newcost) {
-            if (findId(newID, listofproducts)) {
-                listofproducts.push({ id: newID, name: newname, price: newcost, stock: newstock, minstock: newminstock });
-                localStorage.setItem("productslistSave", JSON.stringify(listofproducts)); //Guardo la nueva vista
-
-            } else {
-                alert("Ya existe un producto con este ID, ingrese otro.")
-            }
-        } else {
-            alert("Complete todos los campos.");
-        }
-
-
-    });
-
+   
 });
-
-function findId(ID, list) {
-    let status = true;
-    for (let product of list) {
-        if (ID == product.id) {
-            status = false;
+ function findId(ID, list) { //Función que devuelve true si no hay coincidencias con ID's existentes
+        let status = true;
+        for (let product of list) {
+            if (ID == product.id) {
+                status = false;
+            }
         }
+        return status;
     }
-    return status;
-}
-
-
 
--- js/script.js
+++ js/script.js
@@ -16,12 +16,24 @@ function alertData(theindex) {
     let list = JSON.parse(localStorage.getItem("productslistSave")); //Extraigo lista de productos cargada
     if (list[theindex].stock < list[theindex].minstock) {
         classname = "color-red";
-    } else if (list[theindex].stock < list[theindex].minstock + 200) {
+    } else if (list[theindex].stock < list[theindex].minstock*1.2) { //Al 50% del stock mínimo
         classname = "color-yellow";
     } else {
         classname = "color-green";
     }
     return classname
+}
+function deleteData(index) { //Función que se ejecuta cuando se cliquea en el ícono de basura
+    let list = JSON.parse(localStorage.getItem("productslistSave")); //Extraigo lista de productos cargada
+    list.splice(index, 1); //Elimino el elemento en el ícono indicado y redefino lista 
+    if (confirm(`¿Está seguro que desea eliminar ${list[index].name}?`)){
+        localStorage.setItem("productslistSave", JSON.stringify(list));
+        let tbody = document.getElementById("toinformation");
+        tbody.innerHTML = "";
+        showData() //Muestro la nueva lista
+    }
+
+    
 }
 const showData = () => {
     let tbody = document.getElementById("toinformation");
@@ -30,15 +42,17 @@ const showData = () => {
 
     for (var i = 0; i < list.length; i++) {
         alertData(i);
-        tbody.innerHTML += `<tr><td>${list[i].name}</td><td>${list[i].id}</td><td>${JSON.stringify((list[i].id)).substring(2, 0)}</td><td>$${list[i].price}</td><td class="${classname}">${list[i].stock}</td></tr>`;
+        tbody.innerHTML += `<tr><td>${list[i].name}</td><td>${list[i].id}</td><td>${JSON.stringify((list[i].id)).substring(2, 0)}</td><td>$${list[i].price}</td><td  class="${classname}">${list[i].stock}</td><td onclick="deleteData(${i})" id="trash${i}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+      </svg></td></tr>`;
     }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-
-
+    
     document.getElementById("ProductDescription").value = "";
-    document.getElementById("subtotal").value = "";
+    document.getElementById("count").value = "";
     showData();
 
 
@@ -51,10 +65,10 @@ const buttonIngresar = () => { //Al cliquear en botón de Guardar
     let tbody = document.getElementById("toinformation");
     let type;
     let idproduct = document.getElementById("ProductDescription").value; //Id del producto
-    let subtotal = document.getElementById("subtotal").value;
+    let count = document.getElementById("count").value;
 
     let list = JSON.parse(localStorage.getItem("productslistSave")); //Extraigo lista de productos cargada
-    if (idproduct && subtotal) { //Si datos completos
+    if (idproduct && count) { //Si datos completos
         for (var i = 0; i < list.length; i++) { //Recorro lista para buscar mismo ID
 
             if (parseInt(idproduct) == list[i].id) {
@@ -62,22 +76,21 @@ const buttonIngresar = () => { //Al cliquear en botón de Guardar
 
                 if (document.getElementById('venta').checked) {
 
-                    if (list[i].stock - parseInt(subtotal) >= 0) {
+                    if (list[i].stock - parseInt(count) >= 0) {
                         type = document.getElementById('venta').value;
-                        list[i].stock = list[i].stock - parseInt(subtotal); //Si es una resto cantidad de vendidos
+                        list[i].stock = list[i].stock - parseInt(count); //Si es una resto cantidad de vendidos
                     } else {
                         alert("Stock insuficiente")
                     }
                 } else {
                     type = document.getElementById('compra').value;
-                    list[i].stock = list[i].stock + parseInt(subtotal); //Sino sumo
+                    list[i].stock = list[i].stock + parseInt(count); //Sino sumo
                 }
                 tbody.innerHTML = "";
 
                 localStorage.setItem("productslistSave", JSON.stringify(list)); //Guardo la nueva vista
 
                 //Gestión de alertas;
-                alertData(index);
                 showData();
 
             }
